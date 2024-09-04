0a82613e1749140e6de1daac26059f6e4bfe3a82
alessandros-o <olive.alessandros@gmail.com>
2024-09-04 17:11:20 -0300

    add string validator

--- src/main/java/br/com/alessandro/webflux_course/model/request/UserRequest.java
+++ src/main/java/br/com/alessandro/webflux_course/model/request/UserRequest.java
@@ -1,17 +1,23 @@
 package br.com.alessandro.webflux_course.model.request;
 
+import br.com.alessandro.webflux_course.validator.TrimString;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRequest(
 
+        @TrimString
         @Size(min = 3, max = 50, message = "must be between 3 and 50 characters")
         @NotBlank(message = "must not be null or empty")
         String name,
+
+        @TrimString
         @Email(message = "invalid email")
         @NotBlank(message = "must not be null or empty")
         String email,
+
+        @TrimString
         @Size(min = 3, max = 10, message = "must be between 3 and 10 characters")
         @NotBlank(message = "must not be null or empty")
         String password
---
+++ src/main/java/br/com/alessandro/webflux_course/validator/TrimStringValidator.java
@@ -0,0 +1,15 @@
+package br.com.alessandro.webflux_course.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import static java.util.Objects.isNull;
+
+public class TrimStringValidator implements ConstraintValidator<TrimString, String> {
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+
+        return isNull(value) || value.trim().length() == value.length();
+    }
+}
